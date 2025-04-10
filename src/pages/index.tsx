cad4c5fdc98dac6a5a7b9a8adce2cf2ced654695
lethelee <lelethelee@gmail.com>
2025-04-11 00:04:43 +0800

    remove some code lines

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,6 @@
-import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
-import { redirect } from "next/dist/server/api-utils";
-import next from "next";
-import NaveMenu from "./components/navmenu";
 import Layout from "./components/layout";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
